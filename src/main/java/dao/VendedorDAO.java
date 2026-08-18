52a239a8593409c88d40b4ae08f7b27f435fa705
Laboratório FIAP <mafe.caldeira@gmail.com>
2026-08-18 11:38:45 -0300

    Producao do projeto 2

---
+++ src/main/java/dao/VendedorDAO.java
@@ -0,0 +1,30 @@
+package dao;
+
+import factory.ConnectionFactory;
+import model.Vendedor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class VendedorDAO implements GenericDAO<Vendedor, Integer> {
+    @Override
+    public void inserir(Vendedor entidade) {
+        String sql = "insert into java_vendedor(nome) values(?)";
+
+        try(Connection connection = ConnectionFactory.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, entidade.getNome());
+            ps.execute();
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public List<Vendedor> listar() {
+        return List.of();
+    }
+}
